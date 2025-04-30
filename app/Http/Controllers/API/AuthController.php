a3ee32d1522b596fc97dead09dc53ae7c6366c4a
Michelle Di Terlizzi <michelle.diterlizzi98@gmail.com>
2025-04-30 12:46:21 +0200

    Register controller and route implemented

--- app/Http/Controllers/API/AuthController.php
+++ app/Http/Controllers/API/AuthController.php
@@ -3,9 +3,46 @@
 namespace App\Http\Controllers\API;
 
 use App\Http\Controllers\Controller;
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
 
 class AuthController extends Controller
 {
-    //
+    public function register(Request $request)
+    {
+        // Validate the request data
+        $validator = Validator::make($request->all(), [
+            'name' => 'required|string|max:255',
+            'email' => 'required|string|email|max:255|unique:users',
+            'password' => 'required|string|min:8',
+            'confirm_password' => 'required|string|same:password',
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json([
+                'status' => 0,
+                'message' => 'Validation Error',
+                'errors' => $validator->errors()->all(),
+            ]);
+        }
+
+        $user = User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => bcrypt($request->password),
+        ]);
+
+        $response = [];
+        $response['user'] = $user->name;
+        $response['token'] = $user->createToken('MyApp')->accessToken;
+        $response['email'] = $user->email;
+
+        return response()->json([
+            'status' => 1,
+            'message' => 'User registered successfully',
+            'data' => $response,
+        ]);
+    }
+
 }
