2a8a0f68c9ed06eccb489ff801f11b509fab9248
Michelle Di Terlizzi <michelle.diterlizzi98@gmail.com>
2025-05-03 19:39:33 +0200

    method update event created

--- app/Http/Controllers/API/EventController.php
+++ app/Http/Controllers/API/EventController.php
@@ -7,6 +7,7 @@ use Illuminate\Http\Request;
 use App\Models\Event;
 use Illuminate\Support\Facades\Validator;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Storage;
 
 class EventController extends Controller
 {
@@ -33,7 +34,7 @@ class EventController extends Controller
             'title' => 'required|string|max:255',
             'address' => 'required|string|max:255',
             'event_date' => 'required|date_format:Y-m-d\TH:i',
-            'price' => 'nullable|numeric',
+            'price' => 'required_if:is_free,0|nullable|numeric',
             'is_free' => 'required|boolean',
             'description' => 'required|string|min:50',
             'image' => 'nullable|image|mimes:jpg,png,jpeg|max:5120',
@@ -45,6 +46,11 @@ class EventController extends Controller
                 'message' => 'Validation Error',
                 'errors' => $validator->errors()->all(),
             ], 422);
+        }
+
+        $eventData = $request->all();
+        if ($eventData['is_free']) {
+            $eventData['price'] = null;
         }
 
         $event = new Event([
@@ -84,7 +90,53 @@ class EventController extends Controller
      */
     public function update(Request $request, string $id)
     {
-        //
+        $event = Event::findOrFail($id);
+
+        $validator = Validator::make($request->all(), [
+        'title' => 'nullable|string|max:255',
+        'address' => 'nullable|string|max:255',
+        'event_date' => 'nullable|date_format:Y-m-d\TH:i',
+        'price' => 'required_if:is_free,0|nullable|numeric',
+        'is_free' => 'nullable|boolean',
+        'description' => 'nullable|string|min:50',
+        'image' => 'nullable|image|mimes:jpg,png,jpeg|max:5120',
+        'category_id' => 'nullable|exists:categories,id'
+    ]);
+
+    if ($validator->fails()) {
+        return response()->json([
+            'message' => 'Validation Error',
+            'errors' => $validator->errors()->all(),
+        ], 422);
+    }
+
+    $isFree = filter_var($request->input('is_free', $event->is_free), FILTER_VALIDATE_BOOLEAN);
+
+$updateData = [
+    'title' => $request->input('title', $event->title),
+    'address' => $request->input('address', $event->address),
+    'event_date' => $request->input('event_date', $event->event_date),
+    'price' => $isFree ? null : $request->input('price', $event->price),
+    'is_free' => $isFree,
+    'description' => $request->input('description', $event->description),
+    'category_id' => $request->input('category_id', $event->category_id),
+];
+
+    $event->update($updateData);
+
+    
+    if ($request->hasFile('image')) {
+        if ($event->image) {
+            Storage::delete($event->image); 
+        }
+        $event->image = $request->file('image')->store('images');
+        $event->save(); 
+    }
+
+    return response()->json([
+        'message' => 'Evento actualizado con éxito!',
+        'event' => $event,
+    ], 200);
     }
 
     /**
