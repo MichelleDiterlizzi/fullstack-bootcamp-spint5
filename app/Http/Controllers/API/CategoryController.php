410a4991e257a859836ceabe598cc4a744d58336
Michelle Di Terlizzi <michelle.diterlizzi98@gmail.com>
2025-05-04 11:22:46 +0200

    index categories method and route added

--- app/Http/Controllers/API/CategoryController.php
+++ app/Http/Controllers/API/CategoryController.php
@@ -3,9 +3,19 @@
 namespace App\Http\Controllers\API;
 
 use App\Http\Controllers\Controller;
+use App\Models\Category;
 use Illuminate\Http\Request;
 
 class CategoryController extends Controller
 {
-    //
+    public function index()
+    {
+        $categories = Category::all();
+
+        return response()->json([
+            'status' => 'success',
+            'message' => 'Categories retrieved successfully',
+            'data' => $categories,
+        ], 200);
+    }
 }
--- routes/api.php
+++ routes/api.php
@@ -4,6 +4,7 @@ use App\Http\Controllers\API\AuthController;
 use App\Http\Controllers\API\EventController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\API\CategoryController;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
@@ -12,6 +13,10 @@ Route::get('/user', function (Request $request) {
 Route::post("register", [AuthController::class, 'register']);
 
 Route::post("login", [AuthController::class, 'login']);
+
+Route::get('/categories', [CategoryController::class, 'index'])->name('categories.index');
+
+Route::get('/events/categories/{id_category}', [EventController::class, 'eventsByCategory'])->name('categories.events');
 
 Route::middleware('auth:api')->group(function () {
     Route::apiResource('events', EventController::class);
